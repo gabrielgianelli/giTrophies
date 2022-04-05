ff491214e2533808c6cf2fce97dfb0a8fa9749f9
Gabriel Gianelli <gabrielgianelli@gmail.com>
2022-04-05 12:50:53 -0300

    test(streak): :white_check_mark: add streak tests to improve code coverage

--- test/unit/DailyContributionList.test.ts
+++ test/unit/DailyContributionList.test.ts
@@ -3,6 +3,8 @@ import { expect } from 'chai';
 import sinon, { SinonSandbox } from 'sinon';
 
 import twoDayStreak from './../mocks/TwoDaysStreak.json';
+import threeDaysStreakIncludingToday from './../mocks/ThreeDaysStreakIncludingToday.json';
+import fourDaysStreakWithMissingDay from './../mocks/FourDaysStreakWithMissingDay.json';
 import sevenDaysMaxStreak from './../mocks/SevenDaysMaxStreak.json';
 import DailyContributionList from '../../src/entities/DailyContributionList';
 import DailyContribution from '../../src/entities/DailyContribution';
@@ -28,6 +30,43 @@ describe('Streak Suite Tests', () => {
         const now = new Date(2022, 3, 1);
         sandbox.useFakeTimers(now.getTime());
         const expected = 2;
+        const result = dailyContributionList.currentStreak;
+        expect(result).to.be.deep.equal(expected);
+    });
+
+    it('should return 0 to current streak given an empty list', () => {
+        const dailyContributionList = new DailyContributionList([]);
+        const now = new Date(2022, 3, 1);
+        sandbox.useFakeTimers(now.getTime());
+        const expected = 0;
+        const result = dailyContributionList.currentStreak;
+        expect(result).to.be.deep.equal(expected);
+    });
+
+    it('should get contribution streak including today if the current day already have contributions', () => {
+        const dailyContributions = threeDaysStreakIncludingToday.list.map(contribution => (
+            new DailyContribution(
+                new Date(contribution.date), 
+                parseInt(contribution.totalContributions)
+        )));
+        const dailyContributionList = new DailyContributionList(dailyContributions);
+        const now = new Date(2022, 3, 1);
+        sandbox.useFakeTimers(now.getTime());
+        const expected = 3;
+        const result = dailyContributionList.currentStreak;
+        expect(result).to.be.deep.equal(expected);
+    });
+
+    it('should break the current streak when a day of the sequence is missing', () => {
+        const dailyContributions = fourDaysStreakWithMissingDay.list.map(contribution => (
+            new DailyContribution(
+                new Date(contribution.date), 
+                parseInt(contribution.totalContributions)
+        )));
+        const dailyContributionList = new DailyContributionList(dailyContributions);
+        const now = new Date(2022, 3, 1);
+        sandbox.useFakeTimers(now.getTime());
+        const expected = 4;
         const result = dailyContributionList.currentStreak;
         expect(result).to.be.deep.equal(expected);
     });
